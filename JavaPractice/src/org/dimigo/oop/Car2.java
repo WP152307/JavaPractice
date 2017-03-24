c75d85be9915089dcecd32189463335e1ee00bb8
gimselin <gimselin@218.38.137.28>
2017-03-24 15:30:07 +0900

    실습과제4-2 제출

--- JavaPractice/src/org/dimigo/oop/Car2.java
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -16,12 +16,24 @@ package org.dimigo.oop;
  * @version : 1.0
  *
  */
-public class Car {
+public class Car2 {
 	private String company = "현대자동차";
 	private String model = "제네시스";
 	private String color = "검정색";
 	private int maxSpeed = 255;
 	private int price = 50000000;
+	
+	public Car2(){
+		
+	}
+	
+	public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, int newPrice){
+		company = newCompany;
+		model = newModel;
+		color = newColor;
+		maxSpeed = newMaxSpeed;
+		price = newPrice;
+	}
 	
 	public String getCompany(){
 		return company;
@@ -38,20 +50,5 @@ public class Car {
 	public int getPrice(){
 		return price;	
 	}
-	
-	public void setCompany(String newCompany){
-		company = newCompany;
-	}
-	public void setModel(String newModel){
-		model = newModel;
-	}
-	public void setColor(String newColor){
-		color = newColor;
-	}
-	public void setMaxSpeed(int newMaxSpeed){
-		maxSpeed = newMaxSpeed;
-	}
-	public void setPrice(int newPrice){
-		price = newPrice;
-	}
+
 }
