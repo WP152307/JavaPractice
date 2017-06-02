e9d2ec1ce28ea9f47ccbca0b25e8a3e3af1eb33d
kimselin <kimselin@218.38.137.28>
2017-06-02 15:19:51 +0900

    실습과제14 제출

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,29 @@
+package org.dimigo.exception;
+
+public class Movie {
+
+	private String title;
+	private int limitAge;
+
+	public Movie(String title, int limitAge) {
+		this.title = title;
+		this.limitAge = limitAge;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public int getLimitAage() {
+		return this.limitAge;
+	}
+
+	public void buyTicket(int age) throws Exception{
+		if(age<limitAge){
+			throw new Exception(title + "은/는 " + limitAge + "세 이상 관람가입니다.");
+		} else{
+			System.out.println(title + " 즐감하세요.");
+		}
+		
+	}
+}
