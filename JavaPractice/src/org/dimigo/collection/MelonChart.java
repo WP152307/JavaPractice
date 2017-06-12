66fc0a7750dd67cbfd00e9601ca10ed9af1bd2b4
kimselin <kimselin@218.38.137.28>
2017-06-12 15:30:11 +0900

    실습과제 15 제출

---
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -0,0 +1,44 @@
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MelonChart {
+
+	public static void main(String[] args) {
+		List<Music> list = new ArrayList<Music>();
+
+		System.out.println("-- << 멜론 챠트 >> --");
+		list.add(new Music("I LUV IT","PSY"));
+		list.add(new Music("맞지?","언니쓰"));
+		printList(list);
+		
+		System.out.println();
+		System.out.println("-- << 2위 곡 추가 >> --");
+		list.add(1,new Music("SIGNAL","트와이스"));
+		printList(list);
+		
+		System.out.println();
+		System.out.println("-- << 3위 곡 변경 >> --");
+		list.set(2, new Music("팔레트","아이유"));
+		printList(list);
+		
+		System.out.println();
+		System.out.println("-- << 2위 곡 삭제 >> --");
+		list.remove(1);
+		printList(list);
+		
+		System.out.println();
+		System.out.println("-- << 전체 리스트 삭제 >> --");
+		list.clear();
+		printList(list);
+	}
+	public static void printList(List<Music> list){
+		int i=1;
+		for(Music s : list){
+			System.out.println(i +". " +s);
+			i++;
+		}
+	}
+
+}
